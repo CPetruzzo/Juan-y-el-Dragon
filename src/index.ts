6df1e63d9aa8f5431ec2eca450f3fb497620b5ea
CPetruzzo <facu03@gmail.com>
2022-03-16 15:55:03 -0300

    maxim con piso

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
 window.addEventListener("resize", ()=>{
@@ -18,9 +18,8 @@ window.addEventListener("resize", ()=>{
 	const gameWidth= Math.round(app.screen.width * scale);
 	const gameHeight= Math.round(app.screen.height * scale);
 
-	const marginHorizontal= (window.innerWidth - gameWidth) / 2;
-	const marginVertical= (window.innerHeight - gameHeight) / 2;
-
+	const marginHorizontal= Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical= Math.floor((window.innerHeight - gameHeight) / 2);
 
 	app.view.style.width = gameWidth + "px";
 	app.view.style.height = gameHeight + "px";
@@ -41,18 +40,22 @@ Loader.shared.add({url: "./clampy.png", name:"myClampy"});
 Loader.shared.onComplete.add(()=>{
 
 	const clampy: Sprite = Sprite.from("Maxim");
-	const floor: Sprite = Sprite.from("Suelo");
+	const piso: Sprite = Sprite.from("Suelo");
 
 	console.log("Hola mundo!", clampy.height , clampy.width);
 	
 	clampy.anchor.set(0);
-	floor.anchor.set(0);
+	piso.anchor.set(0);
 	
-	clampy.x = app.screen.width / 2;
+	
+	clampy.x = app.screen.width / 3;
 	clampy.y = app.screen.height / 1.7;
 	
+	piso.x = app.screen.width / 10;
+	piso.y = app.screen.height / 10;
+
+	app.stage.addChild(piso);
 	app.stage.addChild(clampy);
-	app.stage.addChild(floor);
 
 })
 

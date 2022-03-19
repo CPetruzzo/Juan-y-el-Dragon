0751d626b17680244b0f39546aa9f5c8f38034bf
CPetruzzo <facu03@gmail.com>
2022-03-19 11:46:32 -0300

    Maxim hablando

--- src/Personajes.ts
+++ src/Personajes.ts
@@ -3,14 +3,8 @@ import { Container, Sprite } from "pixi.js";
 export class Personaje extends Container{
     constructor(){
         super();
-
+        
         const maxim: Sprite = Sprite.from("Maxim");
-
-        maxim.anchor.set(0);
-        maxim.scale.x=1.5;
-        maxim.scale.y=1.5;
-        maxim.position.set(520,430);
-
-    this.addChild(maxim);
+        this.addChild(maxim);
     }
 }
